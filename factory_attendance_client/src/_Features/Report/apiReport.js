fbdf3b8317470f03c211abb22b7c0ed9e3984f6c
alaanajeeb10 <131193760+alaanajeeb10@users.noreply.github.com>
2026-06-22 00:46:26 +0300

    שיפור מבנה headers בקריאת דוח

--- factory_attendance_client/src/_Features/Report/apiReport.js
+++ factory_attendance_client/src/_Features/Report/apiReport.js
@@ -4,7 +4,9 @@ const section = "ATT";
 async function GetReport(worker_id, month, year) {
     let url=`${backURL}/${section}/Report?worker_id=${worker_id}&month=${month}&year=${year}`;
     const response = await fetch(url,{        method: 'GET',
-        headers: {'accept': 'application/json'},
+        headers: {
+            'accept': 'application/json'
+        },
         credentials: 'include'
     });
     if (!response.ok) { throw new Error(`HTTP error! status: ${response.status}`); }
