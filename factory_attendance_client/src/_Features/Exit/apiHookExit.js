f41f83a6c7af31df606e6683120e76ac16942132
alaanajeeb10 <131193760+alaanajeeb10@users.noreply.github.com>
2026-06-22 00:49:15 +0300

    שיפור הודעת שגיאה ברישום יציאה

--- factory_attendance_client/src/_Features/Exit/apiHookExit.js
+++ factory_attendance_client/src/_Features/Exit/apiHookExit.js
@@ -4,8 +4,7 @@ import {AddExit} from "./apiExit.js";
 function useAddExit() {
     const {isLoading:isAdding, mutate:addExit} = useMutation({
         mutationFn: AddExit,
-        onError: async (err) => { console.log("err", err) },
-        onSuccess: ()=>{ alert("היציאה נשמרה בהצלחה") },
+        onError: async (err) => { console.log("Exit Error:", err) },        onSuccess: ()=>{ alert("היציאה נשמרה בהצלחה") },
     });
     return {isAdding, addExit};
 }
